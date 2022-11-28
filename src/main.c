e11d6d1781a198683226d24a3b32f3637b2161ce
Valentin <valentin.chanel@telecomnancy.eu>
2022-11-28 23:11:39 +0100

    ajout de util

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include "explorer.h"
 #include "parameterStruct.h"
+#include "util.h"
 
 int main(int argc, char const *argv[])
 {
-    
+    printf("Size : %ld\n",sizeInByte("1"));
+    printf("Size : %ld\n",sizeInByte("67"));
+    printf("Size : %ld\n",sizeInByte("10k"));
+    printf("Size : %ld\n",sizeInByte("10G"));
+
+    printf("Temps en sec : %f\n", timeInSecond("5"));
+    printf("Temps en sec : %f\n", timeInSecond("5m"));
+    printf("Temps en sec : %f\n", timeInSecond("10h"));
+    printf("Temps en sec : %f\n", timeInSecond("2j"));
+
+
     parameter* param = (parameter*)malloc(sizeof(parameter));
     param->name = "s";
 
